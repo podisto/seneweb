43ddda6d1e603cf1b497885bc813f3a0edc5e100
podisto <el-hadji-omar.ndione@atos.net>
2020-05-07 01:37:55 +0000

    initial commit

---
+++ src/main/java/com/demo/seneweb/SenewebApplication.java
@@ -0,0 +1,13 @@
+package com.demo.seneweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/seneweb/SenewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.seneweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
